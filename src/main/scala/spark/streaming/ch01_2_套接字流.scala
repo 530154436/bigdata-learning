e7d38026468e6eb820aa9827efa112a03fe2f07d
chubin.zheng <chubin.zheng@qizhidao.com>
2024-02-02 15:59:45 +0800

    套接字流_自定义数据源 done

--- src/main/scala/spark/streaming/ch01_2_套接字流.scala
+++ src/main/scala/spark/streaming/ch01_2_套接字流.scala
@@ -11,7 +11,6 @@ import java.nio.file.Paths
  * (2)在nc窗口中随意输入一些单词，监听窗口就会自动获得单词数据流信息，在监听窗口每隔5秒就会打印出词频统计信息。
  */
 object ch01_2_套接字流 {
-
     def main(args: Array[String]): Unit = {
         val hostname = "127.0.0.1"
         val port = 9999
@@ -20,11 +19,8 @@ object ch01_2_套接字流 {
         val ssc = new StreamingContext(sparkConf, Seconds(5))  // 时间间隔为5秒
         ssc.sparkContext.setLogLevel("ERROR")
 
-        // 监听本地目录:
-        val directory = Paths.get(Global.BASE_DIR, "data", "resources").toAbsolutePath
-        val lines = ssc.socketTextStream(hostname, port)
-
         // 统计词频
+        val lines = ssc.socketTextStream(hostname, port)
         val wordCount = lines.flatMap(x => x.split(","))
             .map(x => (x, 1)).reduceByKey((x, y) => x + y)
         wordCount.print()
